ecbca116d63def171aa059f2511059f8dff76ddd
Lael Halawani <lael.halawani.qadev@gmail.com>
2023-11-09 22:51:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='bpe-tokenizer',
+   name='bpe_tokenizer',
    version='1.0',
    packages=find_packages(),
    python_requires='>=3.11',
